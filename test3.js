439c95cc69faa9857d3730e0cbefaeb08e74abf4
root <rekkun1@xkx.me>
2020-01-23 10:20:59 +0000

    lol

---
+++ test3.js
@@ -0,0 +1,20 @@
+const AnyProxy = require('anyproxy');
+const options = {
+  port: 80,
+  webInterface: {
+    enable: true,
+    webPort: 8080
+  },
+  throttle: 10000,
+  forceProxyHttps: false,
+  wsIntercept: false,
+  silent: false
+};
+const proxyServer = new AnyProxy.ProxyServer(options);
+
+proxyServer.on('ready', () => { /* */ });
+proxyServer.on('error', (e) => { /* */ });
+proxyServer.start();
+
+//when finished
+proxyServer.close();
